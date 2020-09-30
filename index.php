eeba448b31f3e039bd0234546f361018f29b892c
Ari Sudana <agung.arisudana@gmail.com>
2020-10-01 03:26:23 +0800

    Melakukan perubahan hapus css pindahkan ke html tambah js

--- index.php
+++ index.php
@@ -1,6 +1,14 @@
 <html>
-<head>Latihan Git</head>
+<head>
+    <title>Latihan Gi</title>
+    <style>
+        body {
+            font-family: arial;
+        }
+    </style>
+</head>
 <body>
     <p>Contoh latihan GIT DTS 2020</p>
+    <script type="text/javascript" src="script.js"></script>
 </body>
 </html>
